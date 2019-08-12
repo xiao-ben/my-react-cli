8d860004bd8de3f321b841fdac389527808d40ff
zhangbenfang <zhangbenfang@zhihu.com>
2019-08-12 16:19:39 +0800

    fix: 修改 autoprefixer 规则

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -56,15 +56,7 @@ function webpackCommonConfigCreator (options) {
                   ident: 'postcss',
                   plugins: loader => [
                     require('postcss-import')({ root: loader.resourcePath }),
-                    autoprefixer({
-                      browsers: [
-                        '>1%',
-                        'last 4 versions',
-                        'Firefox ESR',
-                        'not ie < 9' // React doesn't support IE8 anyway
-                      ],
-                      flexbox: 'no-2009'
-                    })
+                    autoprefixer()
                   ]
                 }
               }
