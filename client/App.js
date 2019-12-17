dcdb21af49bd0f7c47688378b7082f8d8005b810
zhangbenfang <zhangbenfang@zhihu.com>
2019-12-17 18:42:27 +0800

    feat: 遍历🌲

--- client/App.js
+++ client/App.js
@@ -6,17 +6,87 @@ import Home from './pages/Home/Home'
 import styles from './App.css'
 
 if (typeof window !== 'undefined') {
-  // var VConsole = require('vconsole')
+  const VConsole = require('vconsole')
   /* eslint-disable no-unused-vars */
-  // var vConsole = new VConsole()
+  const vConsole = new VConsole()
 }
 
+function addClass (node, str) {
+  if (!new RegExp('(^|\\s+)' + str).test(node.className)) {
+    node.className = node.className + ' ' + str
+  }
+}
+
+function removeClass (node, str) {
+  if (new RegExp('(^|\\s+)' + str).test(node.className)) {
+    node.className = node.className.replace(new RegExp('(^|\\s+)' + str), '')
+  }
+}
+
+let currentSelectDom = null
 class App extends React.Component {
+  state = {
+    domTree: []
+  }
+
+  ref = React.createRef()
+
+  componentDidMount () {
+    const currentPath = []
+    const domTree = []
+    function traversal (node, index = 0) {
+      // 对node的处理
+      if (node && node.nodeType === 1) {
+        currentPath.push(`${node.tagName.toLowerCase()}${index !== undefined ? '-' + index : ''}`)
+        domTree.push({
+          tagName: node.tagName,
+          path: `${currentPath.join('/')}`
+        })
+      }
+      let i = 0
+      const childNodes = Array.from(node.childNodes).filter(node => node && node.nodeType === 1)
+      let item
+      if (childNodes && childNodes.length) {
+        for (; i < childNodes.length; i++, currentPath.pop()) {
+          item = childNodes[i]
+          if (item.nodeType === 1) {
+            // 递归先序遍历子节点
+            traversal(item, i)
+          }
+        }
+      }
+      return domTree
+    }
+    this.setState({
+      domTree: traversal(this.ref.current)
+    })
+  }
+
+  onClick = (e, item) => {
+    let node = document.querySelector('#root')
+    item.path.split('/').forEach(i => {
+      node = node.childNodes[i.split('-')[1]]
+    })
+    if (currentSelectDom) {
+      removeClass(currentSelectDom, styles.zaBorder)
+      currentSelectDom.removeEventListener('click', this.handleZaClick)
+    }
+    addClass(node, styles.zaBorder)
+    currentSelectDom = node
+    node.addEventListener('click', this.handleZaClick)
+    console.log(node, item.path.split('/'), item.path)
+  }
+
+  handleZaClick = e => {
+    console.log('click', e.target.getAttribute('za-data'))
+  }
+
   render () {
     const { setContext, ssrData } = this.props
-    console.log('app-render------')
+    const { domTree } = this.state
+    console.log(domTree, 'app-render------')
     return (
-      <div>
+      <div ref={this.ref}>
         <div className={styles.title} onClick={() => {
           setContext({
             ssrData: {
@@ -24,10 +94,17 @@ class App extends React.Component {
             }
           })
         }}>
-          hot123
+          <ul>
+            <li>1234</li>
+          </ul>
         </div>
-        <div>{JSON.stringify(ssrData)}</div>
+        <div id='demo' za-data={JSON.stringify(ssrData)} >{JSON.stringify(ssrData)}</div>
         <Home />
+        {
+          domTree.map(item => (
+            <div key={item.path} onClick={e => this.onClick(e, item)}>{item.path}</div>
+          ))
+        }
       </div>
     )
   }
